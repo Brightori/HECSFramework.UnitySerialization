d0791d4a0222a38a0472aee749f1d51b444c7970
Brightori <el.brightori@gmail.com>
2025-11-26 02:00:54 +0300

    add: implicit cast to vector3 serialize

--- Vector3Serialize.cs
+++ Vector3Serialize.cs
@@ -5,6 +5,9 @@ namespace HECSFramework.Core
 {
     public partial struct Vector3Serialize
     {
+        public static implicit operator Vector3Serialize (Vector3 data) { return new Vector3Serialize (data); }
+        public static implicit operator Vector3 (Vector3Serialize data) { return data.AsVector; }
+
         public Vector3Serialize(Vector3 source)
         {
             X = source.x;
